666a5cb6018c9fa89556164148948d0e81776902
Alexandre Quessy <alexandre@quessy.net>
2010-07-23 10:55:45 -0400

    initial import

---
+++ autogen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+if [ -d /usr/local/share/aclocal ]; then
+    ACLOCAL_DIR=/usr/local/share/aclocal
+else if [ -d /usr/share/aclocal ]; then
+    ACLOCAL_DIR=/usr/share/aclocal
+    fi
+fi
+
+if [ ! -d use ]; then
+    mkdir use
+fi
+
+aclocal -I $ACLOCAL_DIR
+
+libtoolize --automake --copy
+automake --add-missing --copy
+
+autoconf
